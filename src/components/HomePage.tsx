b57fdad0e4541a312bef30f22c840a78e20ee453
hm-factor <hsmanel@gmail.com>
2021-11-30 11:09:34 -0500

    background opacity and panel style update

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import sample from '../vids/bar_freda_no_audio_hunter.mov';
 
 function HomePage () {
 
@@ -16,9 +17,34 @@ function HomePage () {
                 Doloremque velit aliquam facere excepturi vero assumenda.
               </div>
             </div>
+            {/* <div className="video">
+              <video className='videoTag' autoPlay loop muted>
+                  <source src={sample} type='video/mov' />
+              </video>
+            </div> */}
           </div>
-          <div className="pane">Photos</div>
-          <div className="pane">Merch</div>
+          <div className="pane">
+            <div className="pane-content">
+              <div className="pane-title">
+                Photos
+              </div>
+              <div className="pane-body">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                Doloremque velit aliquam facere excepturi vero assumenda.
+              </div>
+            </div>
+          </div>
+          <div className="pane">
+            <div className="pane-content">
+              <div className="pane-title">
+                Merch
+              </div>
+              <div className="pane-body">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                Doloremque velit aliquam facere excepturi vero assumenda.
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
