6e7430ea8b2e1d42bebed720efc3c4c390cd8170
hm-factor <hsmanel@gmail.com>
2021-12-29 22:03:27 -0500

    setup grid and styled dropdown

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -16,8 +16,21 @@ function HomePage () {
   return (
     <div className="homepage-container">
       <div className={`dropdown ${isModal ? 'modal-on' : ''}`}>
-        <button onClick={()=>closeModal()}></button>
-        photos here
+        photos here.
+        <div className='photo-grid'>
+          <div className='item-1'></div>
+          <div className='item-2'></div>
+          <div className='item-3'></div>
+          <div className='item-4'></div>
+          <div className='item-5'></div>
+          <div className='item-6'></div>
+          <div className='item-7'></div>
+          <div className='item-8'></div>
+          <div className='item-9'></div>
+        </div>
+        <button className='close-modal-btn' onClick={()=>closeModal()}>
+          close.
+        </button>
       </div>
       <div className="panel-container">
         <div className='placeholder-hover'>
