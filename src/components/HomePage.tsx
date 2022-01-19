d0056acee2209f03fe07a470a2cf274d244e92d9
hm-factor <hsmanel@gmail.com>
2022-01-19 00:02:35 -0500

    removing some errors

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,14 +1,5 @@
 import React, { useState } from 'react';
-// import sample from '../vids/bar_freda_no_audio_hunter.mov';
-
 import PhotoBank from './PhotoBank';
-import trash from '../art/trash.png';
-import ep_art from '../art/ep_art.jpg';
-import trees from '../art/trees.jpg';
-import hodge from '../art/hodge.jpg';
-import neg_trees from '../art/neg_trees.jpg';
-import logo_1 from '../art/logo_1.jpg';
-import pool from '../art/pool.jpg';
 
 function HomePage () {
 
@@ -16,66 +7,27 @@ function HomePage () {
   // itll close the opposite modal
   let [isModal, setIsModal] = useState(false);
   let [isShop, setIsShop] = useState(false);
-  let [isGrow, setIsGrow] = useState('');
+  // let [isGrow, setIsGrow] = useState('');
 
   function openModal ():void {
     setIsShop(false);
     setIsModal(true);
   }
 
-  function closeModal ():void {
-    setIsGrow('');
-    setIsModal(false);
-  }
+  // function closeModal ():void {
+  //   setIsGrow('');
+  //   setIsModal(false);
+  // }
 
   function toggleShop ():void {
     setIsModal(false);
     setIsShop(!isShop);
   }
 
-  function photoGrow (alt: string):void {
-    if (!isGrow) {
-      setIsGrow(alt);
-    } else {
-      setIsGrow('');
-    }
-  }
-
   return (
     <div className='homepage-container'>
       {/* TOP MODAL */}
-      {/* <div className={`dropdown-top ${isModal ? 'modal-on' : ''}`}>
-        photos here.
-        <div className='photo-grid'>
-          <div className='photo-square'>
-            <img className={`photo${(isGrow==="trash") ? '-big' : ''}`} src={trash} alt="trash" onClick={()=>photoGrow('trash')}/>
-          </div>
-          <div className='photo-square'>
-            <img className={`photo${(isGrow==="ep_art") ? '-big' : ''}`} src={ep_art} alt="ep_art" onClick={()=>photoGrow('ep_art')}/>
-          </div>
-          <div className='photo-square'>
-            <img className={`photo${(isGrow==="trees") ? '-big' : ''}`} src={trees} alt="trees" onClick={()=>photoGrow('trees')}/>
-          </div>
-          <div className='photo-square'>
-            <img className={`photo${(isGrow==="hodge") ? '-big' : ''}`} src={hodge} alt="hodge" onClick={()=>photoGrow('hodge')}/>
-          </div>
-          <div className='photo-square'>
-            <img className={`photo${(isGrow==="neg_trees") ? '-big' : ''}`} src={neg_trees} alt="neg_trees" onClick={()=>photoGrow('neg_trees')}/>
-          </div>
-          <div className='photo-square'>
-            <img className={`photo${(isGrow==="pool") ? '-big' : ''}`} src={pool} alt="pool" onClick={()=>photoGrow('pool')}/>
-          </div>
-          <div className='photo-square'>
-            <img className={`photo${(isGrow==="logo_1") ? '-big' : ''}`} src={logo_1} alt="logo_1" onClick={()=>photoGrow('logo_1')}/>
-          </div>
-        </div>
-        <button className='close-modal-btn' onClick={closeModal}>
-          close.
-        </button>
-      </div> */}
       <PhotoBank isModal={isModal}/>
-
-
 
       {/* RIGHT MODAL */}
       <div className={`dropdown-right ${isShop ? 'shop-on' : ''}`}>
--- src/components/PhotoBank.tsx
+++ src/components/PhotoBank.tsx
@@ -33,30 +33,30 @@ export default function PhotoBank ({isModal}: PhotoBoothProps) {
             <div className={`dropdown-top ${isModal ? 'modal-on' : ''}`}>
                 photos here.
                 <div className='photo-grid'>
-                <div className='photo-square'>
-                    <img className={`photo${(isGrow==="trash") ? '-big' : ''}`} src={trash} alt="trash" onClick={()=>photoGrow('trash')}/>
-                </div>
-                <div className='photo-square'>
-                    <img className={`photo${(isGrow==="ep_art") ? '-big' : ''}`} src={ep_art} alt="ep_art" onClick={()=>photoGrow('ep_art')}/>
-                </div>
-                <div className='photo-square'>
-                    <img className={`photo${(isGrow==="trees") ? '-big' : ''}`} src={trees} alt="trees" onClick={()=>photoGrow('trees')}/>
-                </div>
-                <div className='photo-square'>
-                    <img className={`photo${(isGrow==="hodge") ? '-big' : ''}`} src={hodge} alt="hodge" onClick={()=>photoGrow('hodge')}/>
-                </div>
-                <div className='photo-square'>
-                    <img className={`photo${(isGrow==="neg_trees") ? '-big' : ''}`} src={neg_trees} alt="neg_trees" onClick={()=>photoGrow('neg_trees')}/>
-                </div>
-                <div className='photo-square'>
-                    <img className={`photo${(isGrow==="pool") ? '-big' : ''}`} src={pool} alt="pool" onClick={()=>photoGrow('pool')}/>
-                </div>
-                <div className='photo-square'>
-                    <img className={`photo${(isGrow==="logo_1") ? '-big' : ''}`} src={logo_1} alt="logo_1" onClick={()=>photoGrow('logo_1')}/>
-                </div>
+                    <div className='photo-square'>
+                        <img className={`photo${(isGrow==="trash") ? '-big' : ''}`} src={trash} alt="trash" onClick={()=>photoGrow('trash')}/>
+                    </div>
+                    <div className='photo-square'>
+                        <img className={`photo${(isGrow==="ep_art") ? '-big' : ''}`} src={ep_art} alt="ep_art" onClick={()=>photoGrow('ep_art')}/>
+                    </div>
+                    <div className='photo-square'>
+                        <img className={`photo${(isGrow==="trees") ? '-big' : ''}`} src={trees} alt="trees" onClick={()=>photoGrow('trees')}/>
+                    </div>
+                    <div className='photo-square'>
+                        <img className={`photo${(isGrow==="hodge") ? '-big' : ''}`} src={hodge} alt="hodge" onClick={()=>photoGrow('hodge')}/>
+                    </div>
+                    <div className='photo-square'>
+                        <img className={`photo${(isGrow==="neg_trees") ? '-big' : ''}`} src={neg_trees} alt="neg_trees" onClick={()=>photoGrow('neg_trees')}/>
+                    </div>
+                    <div className='photo-square'>
+                        <img className={`photo${(isGrow==="pool") ? '-big' : ''}`} src={pool} alt="pool" onClick={()=>photoGrow('pool')}/>
+                    </div>
+                    <div className='photo-square'>
+                        <img className={`photo${(isGrow==="logo_1") ? '-big' : ''}`} src={logo_1} alt="logo_1" onClick={()=>photoGrow('logo_1')}/>
+                    </div>
                 </div>
                 <button className='close-modal-btn' onClick={closeModal}>
-                close.
+                    close.
                 </button>
             </div>
         </>
