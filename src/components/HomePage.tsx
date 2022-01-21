ef4d363329764f166cb752398cfa36706a96949a
hm-factor <hsmanel@gmail.com>
2022-01-20 23:44:12 -0500

    open modal buttons update

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -4,23 +4,23 @@ import MerchShop from'./MerchShop';
 import Panels from './Panels';
 
 function HomePage () {
-  // toggles photo and shop modals -- if one is opened while the other is already opened,
-  // itll close the opposite modal
-  let [isModal, setIsModal] = useState(false);
-  let [isShop, setIsShop] = useState(false);
+    // toggles photo and shop modals -- if one is opened while the other is already opened,
+    // itll close the opposite modal
+    let [isModal, setIsModal] = useState(false);
+    let [isShop, setIsShop] = useState(false);
 
-  return (
-    <div className='homepage-container'>
-      {/* TOP MODAL */}
-      <PhotoBank isModal={isModal} setIsModal={setIsModal}/>
+    return (
+        <div className='homepage-container'>
+            {/* TOP MODAL */}
+            <PhotoBank isModal={isModal} setIsModal={setIsModal}/>
 
-      {/* RIGHT MODAL */}
-      <MerchShop isShop={isShop} setIsShop={setIsShop}/>
+            {/* RIGHT MODAL */}
+            <MerchShop isShop={isShop} setIsShop={setIsShop}/>
 
-      {/* MAIN CONTENT */}
-      <Panels setIsModal={setIsModal} setIsShop={setIsShop} />
-    </div>
-  )
+            {/* MAIN CONTENT */}
+            <Panels setIsModal={setIsModal} setIsShop={setIsShop} />
+        </div>
+    )
 }
 
 export default HomePage;
--- src/components/Panels.tsx
+++ src/components/Panels.tsx
@@ -15,44 +15,44 @@ export default function Panels ({setIsShop, setIsModal}: PanelsProps) {
     }
 
     return ( 
-      <div className="panel-container">
-        <div className='band-name-container'>
-          <p>COHORT <sup>(b)</sup></p>
+        <div className="panel-container">
+            <div className='band-name-container'>
+                <p>COHORT <sup>(b)</sup></p>
+            </div>
+            <div className="panels">
+                <div className="panel">
+                    <div className="panel-content">
+                        <div className="panel-title">
+                            Shows
+                        </div>
+                        <div className="panel-body">
+                            no upcoming shows...
+                        </div>
+                    </div>
+                </div>
+                <div className="panel">
+                    <div className="panel-content">
+                        <div className="panel-title">
+                            Photos
+                        </div>
+                        <div className="panel-body">
+                            <p>cohort hive mind art</p>
+                            <button className="open-modal-btn" onClick={openModal}>open.</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="panel">
+                    <div className="panel-content">
+                        <div className="panel-title">
+                            Merch
+                        </div>
+                        <div className="panel-body">
+                            <p>check out the store and buy some goodies</p>
+                            <button className="open-modal-btn" onClick={toggleShop}>open.</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="panels">
-          <div className="panel">
-            <div className="panel-content">
-              <div className="panel-title">
-                Shows
-              </div>
-              <div className="panel-body">
-                no upcoming shows...
-              </div>
-            </div>
-          </div>
-          <div className="panel">
-            <div className="panel-content">
-              <div className="panel-title">
-                Photos
-              </div>
-              <div className="panel-body">
-                cohort hive mind art
-                <button onClick={openModal}>More</button>
-              </div>
-            </div>
-          </div>
-          <div className="panel">
-            <div className="panel-content">
-              <div className="panel-title">
-                Merch
-              </div>
-              <div className="panel-body">
-                check out the store and buy some goodies
-                <button onClick={toggleShop}>More</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     )
 }
