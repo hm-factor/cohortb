ca161038a7dd2a2219f15967ae7b84d3e4b0c2cf
hm-factor <hsmanel@gmail.com>
2022-02-10 11:10:27 -0500

    dependancy bug w typescript

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function AudioPlayer(props: {url:string}) {
   const { url } = props;
