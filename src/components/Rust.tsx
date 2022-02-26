abd10183065505832e1fa7de39585bec0a9889a8
hm-factor <hsmanel@gmail.com>
2022-02-26 12:58:08 -0500

    setup popup for content

--- src/components/Rust.tsx
+++ src/components/Rust.tsx
@@ -1,15 +1,34 @@
+import { useState } from "react"
 
+function Popup(props: {contentType:string}) {
+    let {contentType} = props;
+
+    return (
+        <div className={`popup-content ${contentType ? '' : 'off'}`}>
+            {contentType}
+        </div>
+    )
+}
 
 export default function Rust() {
+
+    let [isContent, setIsContent] = useState('');
+
+    let handleClick = (contentType:string):void => {
+        isContent===contentType ? setIsContent('') : setIsContent(contentType)
+    }
 
     return (
         <div className="rust-main">
             <div className="cb-mobile">cb.</div>
+            <div className="popup">
+                <Popup contentType={isContent}/>
+            </div>
             <div className="rust-nav">
                 <div className="cb">cb.</div>
-                <div className="nav-element">sound</div>
-                <div className="nav-element">photo</div>
-                <div className="nav-element">shows</div>
+                <div className="nav-element" onClick={() => handleClick('sound')}>sound</div>
+                <div className="nav-element" onClick={() => handleClick('photo')}>photo</div>
+                <div className="nav-element" onClick={() => handleClick('shows')}>shows</div>
             </div>
         </div>
     )
