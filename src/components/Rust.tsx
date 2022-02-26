4094755805a480c9f5a342f47e849860ef3da5c0
hm-factor <hsmanel@gmail.com>
2022-02-25 23:26:01 -0500

    style update for mobile

--- src/components/Rust.tsx
+++ src/components/Rust.tsx
@@ -4,6 +4,7 @@ export default function Rust() {
 
     return (
         <div className="rust-main">
+            <div className="cb-mobile">cb.</div>
             <div className="rust-nav">
                 <div className="cb">cb.</div>
                 <div className="nav-element">sound</div>
