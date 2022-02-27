87593d1b0e08bd53402cd541a5e5570897ab9d90
Simon DeBevoise <simondebevoise@gmail.com>
2022-02-27 14:09:46 -0500

    implement basic routing for popups, with active tabs

--- src/components/Rust.tsx
+++ src/components/Rust.tsx
@@ -1,86 +1,34 @@
-import { useState } from "react";
-
-import hodge from '../art/hodge.jpg';
-import logo_1 from '../art/logo_1.jpg';
-import neg_trees from '../art/neg_trees.jpg';
-import pool from '../art/pool.jpg';
-import trees from '../art/trees.jpg';
-
-import blue_rust from '../art/blue_rust.jpg';
-import crane from '../art/crane.jpeg';
-import h_and_j from '../art/h_and_j.jpg';
-import i_hate_this from '../art/i_hate_this.jpg';
-import red from '../art/red.jpg';
-import shake_full from '../art/shake_full.jpg';
+import { Link, Routes, Route, NavLink } from "react-router-dom";
+import { PhotoPopup } from "./popups/PhotoPopup";
+import { InfoPopup } from "./popups/InfoPopup";
+import { SoundPopup } from "./popups/SoundPopup";
 
 
-
-// import trash from '../art/trash.png';
-
-function Popup(props: {contentType:string}) {
-    let {contentType} = props;
-
-    let photos = [
-        blue_rust,
-        hodge,
-        h_and_j, 
-        logo_1, 
-        neg_trees, 
-        pool, 
-        i_hate_this, 
-        crane, 
-        trees, 
-        red, 
-        shake_full
-    ]
-
-    let photoContent = photos.map((el,idx)=>{
-        return (
-            <div className="photo-content">
-                <img src={el} alt={el} className="photo-element"/>
-            </div>
-        )
-    })
+function NavBar() {
+    const displayActive = (isActive: boolean) => (
+        isActive ? "nav-element nav-element-active" : "nav-element"
+    );
 
     return (
-        <div className={`popup-content ${contentType ? '' : 'off'}`}>
-            <div className={`popup-links ${contentType==='sound' ? '' : 'off'}`}>
-                <a href="https://cohortbbb.bandcamp.com/" target="_blank" rel="noreferrer">bandcamp</a>    
-                <a href="https://open.spotify.com/artist/1rnMxenrTZyuttAhlEC5H2" target="_blank" rel="noreferrer">spotify</a>    
-                <a href="https://music.apple.com/us/artist/cohort-b/1609006188" target="_blank" rel="noreferrer">applemusic</a>    
-                <a href="https://www.youtube.com/channel/UCh8srATnzpEeTg2J5yWLslg" target="_blank" rel="noreferrer">youtube</a>    
-            </div>
-            <div className={`popup-photo ${contentType==='photo' ? '' : 'off'}`}>
-                {photoContent}    
-            </div>
-            <div className={`popup-links ${contentType==='shows' ? '' : 'off'}`}>
-                <a href="https://linktr.ee/cohortb" target="_blank" rel="noreferrer">linktree</a>
-                <a href="https://www.instagram.com/cohortbbb/" target="_blank" rel="noreferrer">instagram</a>
-            </div>
-        </div>
+        <nav className="rust-nav">
+            <NavLink to="/" className="cb">cb.</NavLink>
+            <NavLink to="/sound" className="nav-element" >sound</NavLink>
+            <NavLink to="/photos" className="nav-element">photo</NavLink>
+            <NavLink to="/info" className="nav-element">info</NavLink>
+        </nav>
     )
 }
 
 export default function Rust() {
-
-    let [isContent, setIsContent] = useState('');
-
-    let handleClick = (contentType:string):void => {
-        isContent===contentType ? setIsContent('') : setIsContent(contentType)
-    }
-
     return (
         <div className="rust-main">
             <div className="cb-mobile">cb.</div>
-            <div className="content-mobile">
-                <Popup contentType={isContent}/>
-            </div>
-            <div className="rust-nav">
-                <div className="cb">cb.</div>
-                <div className="nav-element" onClick={() => handleClick('sound')}>sound</div>
-                <div className="nav-element" onClick={() => handleClick('photo')}>photo</div>
-                <div className="nav-element" onClick={() => handleClick('shows')}>info</div>
-            </div>
+            <NavBar />
+            <Routes>
+                <Route path="photos" element={<PhotoPopup />} />
+                <Route path="info" element={<InfoPopup />} />
+                <Route path="sound" element={<SoundPopup />} />
+            </Routes>
         </div>
     )
 }
---
+++ src/components/popups/PhotoPopup.tsx
@@ -0,0 +1,43 @@
+import hodge from '../../art/hodge.jpg';
+import logo_1 from '../../art/logo_1.jpg';
+import neg_trees from '../../art/neg_trees.jpg';
+import pool from '../../art/pool.jpg';
+import trees from '../../art/trees.jpg';
+import crane from '../../art/crane.jpeg';
+import h_and_j from '../../art/h_and_j.jpg';
+import i_hate_this from '../../art/i_hate_this.jpg';
+import blue_rust from '../../art/blue_rust.jpg';
+import red from '../../art/red.jpg';
+import shake_full from '../../art/shake_full.jpg';
+
+export function PhotoPopup() {
+    const photos = [
+        blue_rust,
+        hodge,
+        h_and_j,
+        logo_1,
+        neg_trees,
+        pool,
+        i_hate_this,
+        crane,
+        trees,
+        red,
+        shake_full
+    ]
+
+    const photoContent = photos.map((filepath: string) => {
+        return (
+            <div className="photo-content" key={filepath}>
+                <img src={filepath} alt={filepath} className="photo-element" />
+            </div>
+        )
+    })
+
+    return (
+        <div className="popup-content">
+            <div className={`popup-photo photo`}>
+                {photoContent}
+            </div>
+        </div>
+    )
+}
