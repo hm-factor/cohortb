7028b13a1f8f992f5f7ef2d9aec209afa3c888cd
hm-factor <hsmanel@gmail.com>
2022-02-26 19:11:43 -0500

    added some more photos

--- src/components/Rust.tsx
+++ src/components/Rust.tsx
@@ -1,15 +1,38 @@
 import { useState } from "react";
+
 import hodge from '../art/hodge.jpg';
 import logo_1 from '../art/logo_1.jpg';
 import neg_trees from '../art/neg_trees.JPG';
 import pool from '../art/pool.JPG';
 import trees from '../art/trees.JPG';
+
+import blue_rust from '../art/blue_rust.jpg';
+import crane from '../art/crane.jpeg';
+import h_and_j from '../art/h_and_j.jpg';
+import i_hate_this from '../art/i_hate_this.JPG';
+import red from '../art/red.jpg';
+import shake_full from '../art/shake_full.JPG';
+
+
+
 // import trash from '../art/trash.png';
 
 function Popup(props: {contentType:string}) {
     let {contentType} = props;
 
-    let photos = [hodge, logo_1, neg_trees, pool, trees]
+    let photos = [
+        blue_rust,
+        hodge,
+        h_and_j, 
+        logo_1, 
+        neg_trees, 
+        pool, 
+        i_hate_this, 
+        crane, 
+        trees, 
+        red, 
+        shake_full
+    ]
 
     let photoContent = photos.map((el,idx)=>{
         return (
