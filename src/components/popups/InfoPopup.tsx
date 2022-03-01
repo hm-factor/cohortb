1012d798c3c2d457e05c554e7bfd830ca968e6c5
hm-factor <hsmanel@gmail.com>
2022-03-01 10:47:16 -0500

    post-simon cleanup

--- src/components/popups/InfoPopup.tsx
+++ src/components/popups/InfoPopup.tsx
@@ -1,10 +1,10 @@
 export function InfoPopup() {
     return (
         <div className="popup-content">
-        <div className={"popup-links shows"}>
-            <a href="https://linktr.ee/cohortb" target="_blank" rel="noreferrer">linktree</a>
-            <a href="https://www.instagram.com/cohortbbb/" target="_blank" rel="noreferrer">instagram</a>
-        </div>
+            <div className={"popup-links shows"}>
+                <a href="https://linktr.ee/cohortb" target="_blank" rel="noreferrer">linktree</a>
+                <a href="https://www.instagram.com/cohortbbb/" target="_blank" rel="noreferrer">instagram</a>
+            </div>
         </div>
     )
 }
