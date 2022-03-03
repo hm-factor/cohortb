b0900db34e0166dc6cb0a8c6a35f9454d88a3c45
hm-factor <hsmanel@gmail.com>
2022-03-03 18:15:00 -0500

    remove faulty jpg

--- src/components/popups/PhotoPopup.tsx
+++ src/components/popups/PhotoPopup.tsx
@@ -1,6 +1,6 @@
 import hodge from '../../art/hodge.jpg';
 import logo_1 from '../../art/logo_1.jpg';
-import neg_trees from '../../art/neg_trees.JPG';
+// import neg_trees from '../../art/neg_trees.JPG';
 import pool from '../../art/pool.JPG';
 import trees from '../../art/trees.JPG';
 import crane from '../../art/crane.jpeg';
@@ -16,7 +16,7 @@ export function PhotoPopup() {
         hodge,
         h_and_j,
         logo_1,
-        neg_trees,
+        // neg_trees,
         pool,
         i_hate_this,
         crane,
