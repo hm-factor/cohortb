97e4e931923a5f7cc3b9cb9e0e643f7e6e0a9031
hm-factor <hsmanel@gmail.com>
2022-03-17 18:26:56 -0400

    youtube link update

--- src/components/popups/SoundPopup.tsx
+++ src/components/popups/SoundPopup.tsx
@@ -5,7 +5,7 @@ export function SoundPopup() {
                 <a href="https://cohortbbb.bandcamp.com/" target="_blank" rel="noreferrer">bandcamp</a>
                 <a href="https://open.spotify.com/artist/1rnMxenrTZyuttAhlEC5H2" target="_blank" rel="noreferrer">spotify</a>
                 <a href="https://music.apple.com/us/artist/cohort-b/1609006188" target="_blank" rel="noreferrer">applemusic</a>
-                <a href="https://www.youtube.com/channel/UCh8srATnzpEeTg2J5yWLslg" target="_blank" rel="noreferrer">youtube</a>
+                <a href="https://www.youtube.com/channel/UCGsR7NQDx7SvG_Ns94SBOPQ" target="_blank" rel="noreferrer">youtube</a>
             </div>
         </div>
     )
