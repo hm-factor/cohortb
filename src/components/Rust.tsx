2eae74f4abf32f85402842f5cc08415eab17ee15
hm-factor <hsmanel@gmail.com>
2022-03-17 23:06:25 -0400

    mobile styling

--- src/components/Rust.tsx
+++ src/components/Rust.tsx
@@ -1,15 +1,22 @@
 import { Routes, Route, NavLink } from "react-router-dom";
-import { useState } from "react";
+import { MouseEventHandler, useState } from "react";
 import { PhotoPopup } from "./popups/PhotoPopup";
 import { InfoPopup } from "./popups/InfoPopup";
 import { SoundPopup } from "./popups/SoundPopup";
 import TerminalPopup from "./popups/TerminalPopup";
 
-function NavBar() {
+interface navProps {
+    bandTag: String;
+    setBandTag: Function;
+}
+
+function NavBar({bandTag, setBandTag}:navProps) {
+    
+
     return (
         <nav className="dogbite-nav">
             <NavLink to="/" className="cb">cb.</NavLink>
-            <NavLink to="/sound" className="nav-element" >sound</NavLink>
+            <NavLink to="/sound" className="nav-element">sound</NavLink>
             <NavLink to="/photos" className="nav-element">photo</NavLink>
             <NavLink to="/info" className="nav-element">info</NavLink>
         </nav>
@@ -17,14 +24,15 @@ function NavBar() {
 }
 
 export default function Rust() {
-    let [isTerminal, setIsTerminal] = useState(false)
+    let [isTerminal, setIsTerminal] = useState(false);
+    let [bandTag, setBandTag] = useState('');
 
     return (
         <div className="dogbite-main">
-            <div className="cohort-b">COHORT B</div>
+            <div className={`cohort-b ${bandTag}`}>COHORT B</div>
             <div className="cb-mobile">cb.</div>
             <TerminalPopup isTerminal={isTerminal} setIsTerminal={setIsTerminal}/>
-            <NavBar />
+            <NavBar bandTag={bandTag} setBandTag={setBandTag}/>
             <Routes>
                 <Route path="sound" element={<SoundPopup />} />
                 <Route path="photos" element={<PhotoPopup />} />
