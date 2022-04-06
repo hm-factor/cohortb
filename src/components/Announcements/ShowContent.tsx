64e92692e66d1d1721b69433aacdcff0055a995d
hm-factor <hsmanel@gmail.com>
2022-04-06 17:31:55 -0400

    show content clickable

--- src/components/Announcements/ShowContent.tsx
+++ src/components/Announcements/ShowContent.tsx
@@ -1,7 +1,7 @@
 export default function ShowContent() {
     let showInfo = [
         // place, date, link
-        ['Arlenes Grocery', 'April 17th', ''],
+        ['Arlenes Grocery', 'April 17th', 'https://www.arlenesgrocerynyc.com/events/aux-blood-cohort-b-the-variants-appalling-use/'],
         ['Nowherre', 'April 32th', ''],
         ['Arlenes Grocery', 'April 17th', ''],
         ['Nowherre', 'April 32th', ''],
@@ -11,11 +11,15 @@ export default function ShowContent() {
         ['Nowherre', 'April 32th', '']
     ]
 
+    function openLink(link:string) {
+        window.open(link, "_blank")
+    }
+
     return (
         <div className="show-content">
             {showInfo.map( (info) => {
                 return (
-                    <div className="show-info">
+                    <div className="show-info" onClick={()=>openLink(info[2])}>
                         <div>{info[0]}</div>
                         <div>{info[1]}</div>
                     </div>
