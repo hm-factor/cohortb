db1e08d91803e57e0f03dd890624626a803e8cad
hm-factor <hsmanel@gmail.com>
2022-04-14 22:20:58 -0400

    announcement popup styling overhaul

--- src/components/Announcements/Announcements.tsx
+++ src/components/Announcements/Announcements.tsx
@@ -4,24 +4,40 @@ import ReleaseContent from './ReleaseContent';
 
 export default function Announcements() {
     let [content, setContent] = useState('releases');
+    let [exit, setExit] = useState(false);
+
+    function closeAnnouncements() {
+        setExit(true);
+    }
+    function openAnnouncements() {
+        setExit(false);
+    }
 
     return (
-        <div className="announcement-container">
-            <nav className="announcement-nav">
-                <div 
-                    className={`announcement-nav-element ${content==='releases' ? 'nav-selected' : ''}`} 
-                    onClick={()=>setContent('releases')}>Releases</div>
-                <div 
-                    className={`announcement-nav-element ${content==='shows' ? 'nav-selected' : ''}`} 
-                    onClick={()=>setContent('shows')}>Shows</div>
+        <>
+            <nav className={`announcement-open ${exit ? '' : 'off'}`} onClick={openAnnouncements}>
+                announcements
             </nav>
-            <div className="announcement-content">
-                { content === 'releases' ? (
-                    <ReleaseContent/>
-                ) : (
-                    <ShowContent/>
-                )}
+            <div className={`announcement-container ${exit ? 'off' : ''}`}>
+                <nav className="announcement-nav">
+                    <div 
+                        className={`announcement-nav-element ${content==='releases' ? 'nav-selected' : ''}`} 
+                        onClick={()=>setContent('releases')}>Releases</div>
+                    <div 
+                        className={`announcement-nav-element ${content==='shows' ? 'nav-selected' : ''}`} 
+                        onClick={()=>setContent('shows')}>Shows</div>
+                </nav>
+                <div className="announcement-content">
+                    { content === 'releases' ? (
+                        <ReleaseContent/>
+                    ) : (
+                        <ShowContent/>
+                    )}
+                </div>
+                <div className="announcement-footer" onClick={closeAnnouncements}>
+                    Exit.
+                </div>
             </div>
-        </div>
+        </>
     )
 }
