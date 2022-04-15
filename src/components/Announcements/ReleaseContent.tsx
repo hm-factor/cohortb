29936b2f9f76d41a90559d17b1d8061c126d44fb
hm-factor <hsmanel@gmail.com>
2022-04-15 11:27:24 -0400

    announcement container final? styling

--- src/components/Announcements/ReleaseContent.tsx
+++ src/components/Announcements/ReleaseContent.tsx
@@ -1,8 +1,24 @@
 export default function ReleaseContent() {
 
+    let releaseInfo = [
+        ['EP', '1001 Lukewarm USA', 'https://distrokid.com/hyperfollow/cohortb/1001-lukewarm-usa'],
+        ['Single', 'Dog Bite', 'https://cohortbbb.bandcamp.com/track/dog-bite'],
+        ['Single', 'Rust', 'https://cohortbbb.bandcamp.com/track/rust']
+    ]
+
+    function openLink(link:string) {
+        window.open(link, "_blank")
+    }
+
     return (
         <div className="release-content">
-            releases
+            {releaseInfo.map( (info) => {
+                return (
+                    <div className="release-info" onClick={()=>openLink(info[2])}>
+                        <div>{info[0]} : {info[1]}</div>
+                    </div>
+                )
+            })}
         </div>
     )
 }
