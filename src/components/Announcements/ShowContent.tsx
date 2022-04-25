a9c1e42a48c05b9a2b3bd0c0843b68bd785b8372
hm-factor <hsmanel@gmail.com>
2022-04-25 13:45:42 -0400

    remove arlenes show

--- src/components/Announcements/ShowContent.tsx
+++ src/components/Announcements/ShowContent.tsx
@@ -2,7 +2,7 @@ export default function ShowContent() {
     
     // place, date, link
     let showInfo = [
-        ['Arlenes Grocery', 'Sunday, April 17th', 'https://www.arlenesgrocerynyc.com/events/aux-blood-cohort-b-the-variants-appalling-use/'],
+        // ['Arlenes Grocery', 'Sunday, April 17th', 'https://www.arlenesgrocerynyc.com/events/aux-blood-cohort-b-the-variants-appalling-use/'],
         ['Baby\'s All Right', 'Tuesday May 10th', 'https://www.seetickets.us/event/Cohort-B/479978'],
         // ['Purgatory', 'Sunday June 19th', ''],
         // ['Trans Pecos', 'Sunday July 3rd', ''],
