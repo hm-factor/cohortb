8b94c5e8d0dc167a0c5d505e3ebdc8bf8059b4e5
hm-factor <hsmanel@gmail.com>
2022-08-22 18:04:45 -0400

    update show info

--- src/components/Announcements/ShowContent.tsx
+++ src/components/Announcements/ShowContent.tsx
@@ -3,40 +3,28 @@ export default function ShowContent() {
     // place, date, link, active
     let showInfo = [
         {
-            where:'St Vitus', 
-            when:'Thursday June 16th', 
-            how:'https://dice.fm/event/2prxm-smock-cronies-cohort-b-grave-blankets-16th-jun-saint-vitus-bar-new-york-tickets?pid=5456a411&_branch_match_id=711984420778857475&_branch_referrer=H4sIAAAAAAAAA8soKSkottLXz8nMy9ZLyUxO1UvL1Q%2BxSLZMMkpOM0pJMbMvyEyxNTUxNUs0MTQEABKKxDouAAAA', 
-            active: true
-        },
-        {
-            where:'Purgatory', 
-            when:'Sunday June 19th', 
-            how:'https://dice.fm/event/3ed3v-cohort-b-huffers-don-pardo-pseudobliss-19th-jun-purgatory-new-york-tickets', 
-            active: true
-        },
-        {
-            where:'Trans Pecos', 
-            when:'Tuesday June 28', 
-            how:'', 
-            active:false
-        },
-        {
-            where:'Trans Pecos', 
-            when:'Sunday July 3rd', 
-            how:'', 
-            active:false
-        },
-        {
-            where:'Our Wicked Lady', 
-            when:'Tuesday July 12th', 
-            how:'', 
-            active:false
-        },
-        {
             where:'The Broadway', 
-            when:'Thursday August 35th', 
-            how:'', 
-            active:false
+            when:'Thursday August 25th', 
+            how:'https://www.eventbrite.com/e/cindy-cane-w-cohort-b-cronies-tickets-380086076687', 
+            active: true
+        },
+        {
+            where: 'Bonfire Festival',
+            when: 'Sunday August 28th',
+            how: 'https://www.eventbrite.com/e/bonfire-2022-peace-bonfire-tickets-396752556577?aff=ebdssbdestsearch',
+            active: true
+        },
+        {
+            where: 'Bar Freda',
+            when: 'Friday Sept 9th',
+            how: '',
+            active: false
+        },
+        {
+            where: 'Berlin Under A',
+            when: 'Thursday Sept 15th',
+            how: 'https://dice.fm/event/ag2qx-joudy-residency-night-one-15th-sep-berlin-under-a-new-york-tickets',
+            active: true
         },
     ]
 
