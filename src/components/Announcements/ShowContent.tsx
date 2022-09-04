63ff0c8c048a21add24b671b5064f0ef317bc688
hm-factor <hsmanel@gmail.com>
2022-09-03 23:01:13 -0400

    update showInfo

--- src/components/Announcements/ShowContent.tsx
+++ src/components/Announcements/ShowContent.tsx
@@ -3,24 +3,6 @@ export default function ShowContent() {
     // place, date, link, active
     let showInfo = [
         {
-            where:'The Broadway', 
-            when:'Thursday August 25th', 
-            how:'https://www.eventbrite.com/e/cindy-cane-w-cohort-b-cronies-tickets-380086076687', 
-            active: true
-        },
-        {
-            where: 'Bonfire Festival',
-            when: 'Sunday August 28th',
-            how: 'https://www.eventbrite.com/e/bonfire-2022-peace-bonfire-tickets-396752556577?aff=ebdssbdestsearch',
-            active: true
-        },
-        {
-            where: 'Bar Freda',
-            when: 'Friday Sept 9th',
-            how: '',
-            active: false
-        },
-        {
             where: 'Berlin Under A',
             when: 'Thursday Sept 15th',
             how: 'https://dice.fm/event/ag2qx-joudy-residency-night-one-15th-sep-berlin-under-a-new-york-tickets',
