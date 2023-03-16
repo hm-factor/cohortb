0eb1f22cd0ee462d3bedfa324f9b820e9b6876d7
Hunter Manel <hunter.manel@wpengine.com>
2023-03-16 19:28:27 -0400

    update show and release content

--- src/components/Announcements/Announcements.tsx
+++ src/components/Announcements/Announcements.tsx
@@ -1,25 +1,31 @@
 import { useState } from "react";
-import ShowContent from './ShowContent';
-import ReleaseContent from './ReleaseContent';
+import ShowContent from "./ShowContent";
+import ReleaseContent from "./ReleaseContent";
 
 export default function Announcements() {
-    let [content, setContent] = useState('releases');
+  let [content, setContent] = useState("shows");
 
-    return (
-        <div className='announcement-container'>
-            <nav className="announcement-nav">
-                <div 
-                    className={`announcement-nav-element ${content==='releases' ? 'nav-selected' : ''}`} 
-                    onClick={()=>setContent('releases')}>RELEASES</div>
-                <div 
-                    className={`announcement-nav-element ${content==='shows' ? 'nav-selected' : ''}`} 
-                    onClick={()=>setContent('shows')}>SHOWS</div>
-            </nav>
-            { content === 'releases' ? (
-                <ReleaseContent/>
-            ) : (
-                <ShowContent/>
-            )}
+  return (
+    <div className="announcement-container">
+      <nav className="announcement-nav">
+        <div
+          className={`announcement-nav-element ${
+            content === "shows" ? "nav-selected" : ""
+          }`}
+          onClick={() => setContent("shows")}
+        >
+          SHOWS
         </div>
-    )
+        <div
+          className={`announcement-nav-element ${
+            content === "releases" ? "nav-selected" : ""
+          }`}
+          onClick={() => setContent("releases")}
+        >
+          RELEASES
+        </div>
+      </nav>
+      {content === "releases" ? <ReleaseContent /> : <ShowContent />}
+    </div>
+  );
 }
--- src/components/Announcements/ShowContent.tsx
+++ src/components/Announcements/ShowContent.tsx
@@ -1,42 +1,37 @@
 export default function ShowContent() {
-    
-    // place, date, link, active
-    let showInfo = [
-        {
-            where: 'Berlin Under A',
-            when: 'Thursday Sept 15th',
-            how: 'https://dice.fm/event/ag2qx-joudy-residency-night-one-15th-sep-berlin-under-a-new-york-tickets',
-            active: true
-        },
-    ]
+  // place, date, link, active
+  let showInfo = [
+    {
+      where: "Heaven Can Wait",
+      when: "Sunday March 19",
+      how: "https://heavencanwaitnyc.com/event/12959315/cohort-b-butthole-university-no-room-at-the-morgue-and-cometa-negra/",
+      active: true,
+    },
+  ];
 
-    function openLink(link:string) {
-        window.open(link, "_blank")
-    }
+  function openLink(link: string) {
+    window.open(link, "_blank");
+  }
 
-    return (
-        <div className="show-content">
-            {showInfo.map( (info) => {
-                const {where, when, how, active} = info;
+  return (
+    <div className="show-content">
+      {showInfo.map((info) => {
+        const { where, when, how, active } = info;
 
-                let content = active ? (
-                    <div className='show-info' onClick={()=>openLink(how)}>
-                        <div>{where}</div>
-                        <div>{when}</div>
-                    </div>
-                ) : (
-                    <div className='noClick'>
-                        <div>{where}</div>
-                        <div>{when}</div>
-                    </div>
-                );
-                
-                return (
-                    <>
-                        {content}
-                    </>
-                )
-            })}
-        </div>
-    )
+        let content = active ? (
+          <div className="show-info" onClick={() => openLink(how)}>
+            <div>{where}</div>
+            <div>{when}</div>
+          </div>
+        ) : (
+          <div className="noClick">
+            <div>{where}</div>
+            <div>{when}</div>
+          </div>
+        );
+
+        return <>{content}</>;
+      })}
+    </div>
+  );
 }
