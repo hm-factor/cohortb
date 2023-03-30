4c9b2fe8f9e966f63f04f02566a84542c999773d
Hunter Manel <hunter.manel@wpengine.com>
2023-03-29 22:22:16 -0400

    update shows

--- src/components/Announcements/ShowContent.tsx
+++ src/components/Announcements/ShowContent.tsx
@@ -2,10 +2,10 @@ export default function ShowContent() {
   // place, date, link, active
   let showInfo = [
     {
-      where: "Heaven Can Wait",
-      when: "Sunday March 19",
-      how: "https://heavencanwaitnyc.com/event/12959315/cohort-b-butthole-university-no-room-at-the-morgue-and-cometa-negra/",
-      active: true,
+      where: "Mainline Mosh",
+      when: "Saturday April 1",
+      how: "",
+      active: false,
     },
   ];
 
