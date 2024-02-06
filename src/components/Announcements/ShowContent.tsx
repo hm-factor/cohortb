73defac1f6b5cd0602a84a92fd37dda932b76480
Hunter Manel <hsmanel@gmail.com>
2024-02-05 23:03:27 -0500

    show update feb 24

--- src/components/Announcements/ShowContent.tsx
+++ src/components/Announcements/ShowContent.tsx
@@ -4,9 +4,21 @@ export default function ShowContent() {
   // place, date, link, active
   let showInfo = [
     {
-      where: "Knitting Factory, NY",
-      when: "Wednesday Dec 6",
-      how: "https://www.knittingfactory.com/tm-event/libby-quinn-cold-court-cohort-b/",
+      where: "Trans Pecos, NY",
+      when: "Friday Feb 9",
+      how: "https://www.venuepilot.co/events/94547/orders/new#/",
+      active: true,
+    },
+    {
+      where: "The Broadway, NY",
+      when: "Thursday Feb 15",
+      how: "https://www.eventbrite.com/e/mother-cell-w-fatboi-sharif-cronies-cohort-b-tickets-798935687147?aff=oddtdtcreator",
+      active: true,
+    },
+    {
+      where: "Saint Vitus, NY",
+      when: "Friday Mar 1",
+      how: "https://dice.fm/event/axvpd-couch-slut-splinternet-cohort-b-eaten-by-bees-1st-mar-saint-vitus-bar-new-york-tickets?pid=5456a411&_branch_match_id=1090979828181667380&_branch_referrer=H4sIAAAAAAAAA8soKSkottLXz8nMy9ZLyUxO1UvL1Q9NtkhJNLAwskg2SLMvyEyxNTUxNUs0MTQEANx5rJkuAAAA",
       active: true,
     },
   ];
