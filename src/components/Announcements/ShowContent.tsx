d0339c2943470ef61f9527a1ccb76e93f79a98e3
Hunter Manel <hsmanel@gmail.com>
2024-05-02 17:35:04 -0400

    show update june

--- src/components/Announcements/ShowContent.tsx
+++ src/components/Announcements/ShowContent.tsx
@@ -15,6 +15,24 @@ export default function ShowContent() {
       how: "https://www.thetranspecos.com/cal/2024/5/16/wiring-naras-room-screenager-cohort-b",
       active: true,
     },
+    {
+      where: "The Garrison, Toronto",
+      when: "Tues June, 25",
+      how: "https://dice.fm/event/25yr6-lip-critic-25th-jun-the-garrison-toronto-tickets?pid=530cb286&_branch_match_id=1090979828181667380&_branch_referrer=H4sIAAAAAAAAA8soKSkottLXz8nMy9ZLyUxO1UvL1S8ysDBKNE4zSE41TrYvyEyxNTU2SE4ysjADAKCkrJIuAAAA",
+      active: true,
+    },
+    {
+      where: "The 27 Club, Ottawa",
+      when: "Wed June, 26",
+      how: "https://www.ticketweb.ca/event/lip-critic-los3r-scrip-issue-the-27-club-tickets/13544513?irgwc=1&clickid=yg8SaI2XaxyNTy8wNYVfgyhTUkHTMhWAw0TJSU0&camefrom=CFC_BUYAT_219208&impradid=219208&REFERRAL_ID=tmfeedbuyat219208&wt.mc_id=aff_BUYAT_219208&utm_source=219208-Bandsintown&impradname=Bandsintown&utm_medium=affiliate&ircid=4272",
+      active: true,
+    },
+    {
+      where: "O'Briens Pub, Boston",
+      when: "Thurs June, 27",
+      how: "https://obrienspubboston.com/show/lip-critic/",
+      active: true,
+    },
   ];
 
   function openLink(link: string) {
