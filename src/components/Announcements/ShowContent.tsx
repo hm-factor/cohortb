6576938705e3357ef6cb9db83d0d738a616f46df
Hunter Manel <hsmanel@gmail.com>
2024-09-01 11:29:28 -0400

    update shows content

--- src/components/Announcements/ShowContent.tsx
+++ src/components/Announcements/ShowContent.tsx
@@ -4,33 +4,21 @@ export default function ShowContent() {
   // place, date, link, hover
   let showInfo = [
     {
-      where: "El Dorado, Troy",
-      when: "Sun June, 23",
-      how: "",
-      active: false,
-    },
-    {
-      where: "The Garrison, Toronto",
-      when: "Tues June, 25",
-      how: "https://dice.fm/event/25yr6-lip-critic-25th-jun-the-garrison-toronto-tickets?pid=530cb286&_branch_match_id=1090979828181667380&_branch_referrer=H4sIAAAAAAAAA8soKSkottLXz8nMy9ZLyUxO1UvL1S8ysDBKNE4zSE41TrYvyEyxNTU2SE4ysjADAKCkrJIuAAAA",
-      active: true,
-    },
-    {
-      where: "The 27 Club, Ottawa",
-      when: "Wed June, 26",
-      how: "https://www.ticketweb.ca/event/lip-critic-los3r-scrip-issue-the-27-club-tickets/13544513?irgwc=1&clickid=yg8SaI2XaxyNTy8wNYVfgyhTUkHTMhWAw0TJSU0&camefrom=CFC_BUYAT_219208&impradid=219208&REFERRAL_ID=tmfeedbuyat219208&wt.mc_id=aff_BUYAT_219208&utm_source=219208-Bandsintown&impradname=Bandsintown&utm_medium=affiliate&ircid=4272",
-      active: true,
-    },
-    {
-      where: "O'Briens Pub, Boston",
-      when: "Thurs June, 27",
-      how: "https://obrienspubboston.com/show/lip-critic/",
-      active: true,
-    },
-    {
       where: "Gold Sounds, NYC",
-      when: "Sat June, 29",
-      how: "https://dice.fm/event/k66pqo-cohort-b-wince-secret-child-eater-29th-jun-gold-sounds-new-york-tickets?lng=en-US",
+      when: "Sat Sept, 14",
+      how: "https://dice.fm/event/l8qowb-unsee-cohort-b-shiverboard-cometa-negra-14th-sep-gold-sounds-new-york-tickets?lng=en-US",
+      active: true,
+    },
+    {
+      where: "The Wood Shop, NYC",
+      when: "Fri Sept, 20",
+      how: "https://dice.fm/event/8ee7y5-trophy-husband-servants-holiem-cohort-b-20th-sep-the-wood-shop-new-york-city-tickets?lng=en-US",
+      active: true,
+    },
+    {
+      where: "Main Drag, NYC",
+      when: "Fri Oct, 11",
+      how: "https://maindragmusic.com/collections/live-at-main-drag/products/10-11-24-a-deer-a-horse-cohort-b-special-guest-tba",
       active: true,
     },
   ];
